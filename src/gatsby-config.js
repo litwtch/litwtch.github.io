14451ba7cee0f7789ff7377d9a01a7f21e1444f7
Lauren <7035065+litwtch@users.noreply.github.com>
2024-09-16 09:41:47 -0400

    Add files via upload

---
+++ src/gatsby-config.js
@@ -0,0 +1,45 @@
+module.exports = {
+    siteMetadata: {
+      author: `@ottiliaw`,
+      home: {
+        title: `Ottilia Westerlund`,
+        description: `<3`,
+      }
+    },
+    plugins: [
+      `gatsby-plugin-react-helmet`,
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          name: `images`,
+          path: `${__dirname}/src/images`,
+        },
+      },
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          name:`markdown-pages`,
+          path: `${__dirname}/_data`,
+        },
+      },
+      { resolve: `gatsby-transformer-remark` },
+      `gatsby-transformer-sharp`,
+      `gatsby-plugin-sharp`,
+      {
+        resolve: `gatsby-plugin-manifest`,
+        options: {
+          name: `gatsby-starter-default`,
+          short_name: `starter`,
+          start_url: `/`,
+          background_color: `#663399`,
+          theme_color: `#663399`,
+          display: `minimal-ui`,
+          icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        }},
+        `gatsby-plugin-sass`
+      // siteURL is a must for sitemap generation
+      // this (optional) plugin enables Progressive Web App + Offline functionality
+      // To learn more, visit: https://gatsby.dev/offline
+      // `gatsby-plugin-offline`,
+    ],
+  }
